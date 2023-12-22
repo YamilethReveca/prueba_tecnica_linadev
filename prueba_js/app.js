81507686624510550f32cd51d98f01ca6420c8a4
Yamileth Reveca <yamilethcaldera@gmail.com>
2023-12-22 19:19:46 -0300

    js y estilo css

--- prueba_js/app.js
+++ prueba_js/app.js
@@ -3,13 +3,43 @@
 
 const checkOk = ['✅', '✅', '❎', '❎', '❎'];
 
+function verificar(arg){
+
+return arg==="✅"
+
+}
+
+//console.log(checkOk.every(verificar))
+
+
 //Ejercicio 2: Rellena los elementos del array con dinero
 
 const bolsasdinero = ['', '', ''];
 
+const dinero = bolsasdinero.fill('💰')
+
+
+//for(let i= 0; i<dinero.length; i++){
+
+    //bolsasdinero.push(dinero[i])
+
+    //return bolsasdinero;
+
+//
+
+//console.log(dinero);
+
+
+
+
 //Teniendo en cuenta el sgte arreglo:
 
 const precios = [1045.0, 234, 456.98, 238.98, 3455, 234, 1045.0, 234];
+
+
+
+
+
 
 //Ejercicio 3: Deberá realizar el cálculo de : promedio, valor máximo, suma total de los precios, suma de los precios que son menores a 1500 dólares.
 
@@ -34,5 +64,11 @@ const personas = [{
 ]
 
 //1. Devolver los nombres es mayuscula.
+
+let gente= personas.nombre;
+
+console.log(gente);
+
+
 //2. Cambiar el apellido "Guerrero" por "López".
 //3. Insertar el campo "email" en ambos objetos.
